f72b75a9cbad7a451c50738e08ffaaa88c4ef55e
Megha <megha.borse19@gmail.com>
2024-11-30 15:13:52 +0530

    initial commit

---
+++ src/main/java/com/example/task2/CentimeTask2Application.java
@@ -0,0 +1,14 @@
+package com.example.task2;
+
+import org.springframework.boot
+		.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentimeTask2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentimeTask2Application.class, args);
+	}
+
+}
