0f7c73d0658000a3ab3946f2e8520a0d6f883e4b
Megha <megha.borse19@gmail.com>
2024-11-30 17:23:55 +0530

    logic corrected

--- src/main/java/com/example/task2/service/ObjectService.java
+++ src/main/java/com/example/task2/service/ObjectService.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class ObjectService {
@@ -18,11 +19,12 @@ public class ObjectService {
     }
     public List<Map<String, Object>> getNestedObjects() {
         List<ObjectEntity> objectEntities = repository.findAll();
-        
+
+        List<Map<String, Object>> outputList = new ArrayList<>();
         Map<Integer, List<ObjectEntity>> childeObjectEntities = new HashMap<>();
         List<ObjectEntity> parentObjectEntities = new ArrayList<>();
         
-        //get childeObjectEntities & parentObjectEntities
+        //get childeObjectEntities & parentObjectEntities separately
         for (ObjectEntity obj : objectEntities) {
             int parentId = obj.getParentId();
             if (parentId != 0) {
@@ -31,22 +33,22 @@ public class ObjectService {
                     children = new ArrayList<>();
                     childeObjectEntities.put(parentId, children);
                 }
-
                 children.add(obj);
 
             } else {
                 parentObjectEntities.add(obj);
             }
         }
-        List<Map<String, Object>> outputList = new ArrayList<>();
 
-        for (ObjectEntity parent : parentObjectEntities) {
+        for (ObjectEntity parent : parentObjectEntities) {  //iterate through parent one by one
             int paremntid = parent.getId();
-            if (childeObjectEntities.containsKey(paremntid)) {
-                List<ObjectEntity> childEntities = childeObjectEntities.get(paremntid);
+            if (childeObjectEntities.containsKey(paremntid)) {  //  is parentId belong to any child?
+                List<ObjectEntity> childEntities = childeObjectEntities.get(paremntid); // get the childes of given arent
+                List<String> childEntitiesName= childEntities.stream()
+                        .map (s->s.getName()).collect(Collectors.toList());// get names of child
                 Map<String, Object> innerMap = new HashMap<>();
-                innerMap.put("Sub Class", childEntities);  //
-                innerMap.put("Name", parent.getName());
+                innerMap.put("Name", parent.getName());   //update parent name
+                innerMap.put("Sub Class", childEntitiesName);  // update childs
                 outputList.add(innerMap);
             }
 
